324c0a9fa512ef05189ef3e97fff7b1ded98cc96
Andreas Møller <andreas.moller@gmail.com>
2015-10-23 01:25:27 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const rmdir = require('rimraf').sync;
+const _ = require('highland');
 const vinyl = require('vinyl-fs');
+const clone = require('nodegit').Clone.clone;
 const transform = require('./transform');
 const data = {
-  name: 'hello'
+  appName: 'hello',
+  appDescription: ' AAAAAwesome'
 }
 
-vinyl.src('template/**/*')
-  .pipe(transform(data))
-  .pipe(vinyl.dest('app'));
+clone('https://github.com/cullophid/webapp-template', "__tmp__", null)
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      _(vinyl.src('__tmp__/template/**/*')
+        .pipe(transform(data))
+        .pipe(vinyl.dest('app')))
+        .apply(() => resolve());
+    });
+  })
+  .then( () => {
+    rmdir('__tmp__');
+  })
+  .catch(err => {
+    console.log(err);
+  })
